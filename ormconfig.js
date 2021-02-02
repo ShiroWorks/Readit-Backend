513563ccd6d3a410ada2333f00f1c6cc48b8b331
Szabo Vilmos <vili8899s@gmail.com>
2021-02-02 20:59:48 +0100

    added url

--- ormconfig.js
+++ ormconfig.js
@@ -4,6 +4,7 @@ module.exports = {
   type: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  url: process.env.URL,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
