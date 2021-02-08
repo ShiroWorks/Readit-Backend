74dc90b91dc9a41a2d9552934ce3191bf3677837
Szabo Vilmos <vili8899s@gmail.com>
2021-02-09 00:14:28 +0100

    ssl

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -100,7 +100,7 @@ const logout = (_: Request, res: Response) => {
     'Set-Cookie',
     cookie.serialize('token', '', {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === 'production',
+       secure: process.env.NODE_ENV === 'production',
       sameSite: 'none',
       expires: new Date(0),
       path: '/',
